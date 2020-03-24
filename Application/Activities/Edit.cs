e0560dee234febbe23d4c27c0dfa6ba23275f00e
Evan <evanjcook983@gmail.com>
2020-03-24 14:57:48 -0500

    10.9

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
 using Domain;
 using FluentValidation;
 using MediatR;
@@ -43,9 +45,11 @@ namespace Application.Activities
                 public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
                 {
                     var activity =await _context.Activities.FindAsync(request.Id);
-                    if (activity==null)
-                    {throw new Exception("Could not find activity");
-                    }
+                    if(activity== null )
+                    {
+                        throw new RestException(HttpStatusCode.NotFound,new {activity="Not Found"});
+                    };
+                  
 
                     activity.Title      = request.Title      ?? activity.Title      ;
                     activity.Description= request.Description?? activity.Description;

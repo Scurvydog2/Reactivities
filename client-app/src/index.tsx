fa461950b043d2c0df0a3036d699a94da7ac3c87
Evan <evanjcook983@gmail.com>
2020-03-25 17:55:00 -0500

    11.7

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 import "./app/layout/styles.css";
 import * as serviceWorker from "./serviceWorker";
 import App from "./app/layout/App";
+import 'react-widgets/dist/css/react-widgets.css';
+import dateFnsLocalizer from 'react-widgets-date-fns';
 import { Router } from "react-router-dom";
 import {createBrowserHistory} from 'history'
 import 'react-toastify/dist/ReactToastify.min.css'
 import ScrollToTop from "../src/app/layout/ScrollToTop";
+
+dateFnsLocalizer();
+
 export const history=createBrowserHistory();
 ReactDOM.render(
   <Router history={history}>

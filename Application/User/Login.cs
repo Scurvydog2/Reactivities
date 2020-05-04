33f7d37e9af5f709789d0af348c1dd7bc06f740b
Evan <evanjcook983@gmail.com>
2020-05-04 17:12:23 -0500

    12.9 username issue

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -13,7 +13,7 @@ namespace Application.User
 
     public class Login
     {
-        public class Query : IRequest<AppUser>
+        public class Query : IRequest<User>
         {
 
             public string Email { get; set; }
@@ -32,18 +32,18 @@ namespace Application.User
 
 
 
-        public class Handler : IRequestHandler<Query, AppUser>
+        public class Handler : IRequestHandler<Query, User>
         {
-            private readonly UserManager<AppUser> _userManager;
-            private readonly SignInManager<AppUser> _signInManager;
-            public Handler(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+            private readonly UserManager<User> _userManager;
+            private readonly SignInManager<User> _signInManager;
+            public Handler(UserManager<User> userManager, SignInManager<User> signInManager)
             {
                 _signInManager = signInManager;
                 _userManager = userManager;
 
             }
 
-            public async Task<AppUser> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
             {
                 var user = await _userManager.FindByEmailAsync(request.Email);
                 if(user==null)
@@ -53,7 +53,13 @@ namespace Application.User
                 var result = await _signInManager.CheckPasswordSignInAsync(user,request.Password,false);
                 if (result.Succeeded)
                 {
-                    return user;
+                    return new User{
+                        DisplayName=user.DisplayName,
+                        Token="This will be a token",
+                        Username=user.UserName,
+                        Image=null
+
+                    };
                 }
                 throw new RestException(HttpStatusCode.Unauthorized);
             }
